7405c3c706c92609b7e6880de609da05957e05b6
Romano <arcferreira@id.uff.br>
2025-01-30 20:42:20 -0300

    main polish

--- src/main.go
+++ src/main.go
@@ -1,111 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	asset "main/data-access/asset-allocations"
-	"main/data-access/transaction"
-	parsers "main/parsers"
-
-	// transaction "main/data-access/transaction"
 	connectdb "main/data-access"
+	setup "main/setup"
 )
 
-const filePath = "../arquivos-status/"
-const assetSheetFile1 = "carteira-patrimonio-export-2025-01-29_BC.xlsx"
-const assetSheetOwner1 = "Bruna"
-const assetSheetFile2 = "carteira-patrimonio-export-2025-01-29_R.xlsx"
-const assetSheetOwner2 = "Romano"
+const filePathAssets = "../arquivos-status/asset-files/"
+const filePathTransactions = "../arquivos-status/"
 
-const transactionSheetFile1="carteira-export-2025-01-29_BC.xlsx"
-
-
-func setupAssetAllocations(assetSheetFile string, assetOwner string) (bool, error) {
-	allocations, err := parsers.ParseAllSheetsAssetAllocations(filePath+assetSheetFile, assetOwner)
-	if err != nil {
-		log.Fatalf("Error parsing asset allocations: %v", err)
-	}
-
-	// // print allocations
-	// for _, allocation := range allocations {
-	// fmt.Printf("%+v\n", allocation)
-	// }
-
-	// adding allocations to database
-	for _, alcs := range allocations {
-		_, err := asset.AddAssetAllocation(alcs)
-		if err != nil {
-			fmt.Println(err)
-	}
-	}
-	return true, err;
-}
-
-func setupTransactions(transactionSheetFile string) {
-	transactions, err := parsers.ParseTransactionFile(filePath+transactionSheetFile)
-	if err != nil{
-		fmt.Println(err)
-	}
-
-	// // printing the parsed transactions
-	// for _, transaction := range transactions {
-	// 	fmt.Printf("%+v\n", transaction)
-	// }
-
-	// adding the parsed transactions to the database - schema: transactions
-	fmt.Printf("adding all transactions to db\n")
-	for _, trs := range transactions {
-		_, err := transaction.AddTransaction(trs)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
-
+const transactionSheetFile1 = "carteira-export-2025-01-29_BC.xlsx"
 
 func main() {
+	connectdb.ConnectDB()
 
-	connectdb.ConnectDB();
+	setup.CalculateAndDisplaySomestuff()
 
-	percentages, err := asset.CalculateAssetTypePercentages()
-	if err != nil{
-		fmt.Println(err)
-	}
-
-	fmt.Println("-------------------------------------------------------")
-
-	for _, per := range percentages {
-		fmt.Printf("Porcentagem: %.2f%%, Balance: R$%.2f -->> %s\n", per.Percentage, per.Balance, per.AssetType)
-	}
-
-	fmt.Println("-------------------------------------------------------")
-
-	assetTypeTotalBalances, err := asset.CalculateAssetTypeTotalBalances()
+	assetFiles, owners, err := setup.GetAllAssetFiles(filePathAssets)
 	if err != nil {
-        fmt.Println(err)
-    }
-
-	totalSum := 0.0
-	for _, totalBalance := range assetTypeTotalBalances {
-		totalSum += totalBalance
+		log.Fatalf("Error getting asset files: %v", err)
 	}
 
-	fmt.Printf("Total Asset: R$%.2f\n", totalSum)
-
-
-	fmt.Println("-------------------------------------------------------")
-	// st1, err := setupAssetAllocations(assetSheetFile1, assetSheetOwner1);
-	// if err != nil {
-	// 	log.Printf("Error setting up allocations for file: %s: ", assetSheetFile1)
-	// }
-	
-	// st2, err := setupAssetAllocations(assetSheetFile2, assetSheetOwner2);
-	// if err != nil {
-	// 	log.Printf("Error setting up allocations for file: %s: ", assetSheetFile2)
-	// }
-
-	// if st1 && st2 {
-	// 	setupTransactions(transactionSheetFile1);
-
-	// }
+	for i, assetFile := range assetFiles {
+		_, err := setup.SetupAssetAllocations(filePathAssets+assetFile, owners[i])
+		if err != nil {
+			log.Printf("Error setting up allocations for file: %s: %v", assetFile, err)
+		}
 	}
+
+	setup.SetupTransactions(filePathTransactions+transactionSheetFile1)
+}
